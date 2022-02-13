18303cf333d289681583acc9cf0c491fd8edf2ec
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 20:06:47 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div class="flex w-auto space-x-4 flex-nowrap">
-        <div class="artboard phone-1 horizontal artboard-demo">
+      <div className="flex w-auto space-x-4 flex-nowrap">
+        <div className="artboard phone-1 horizontal artboard-demo">
           <h2>Start editing to see some magic happen!</h2>
           <p className="font-bold">Hi</p>
         </div>
