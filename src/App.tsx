3589519e06f7784b74c4e46589ed01c3ee9c7ebb
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 21:08:49 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,7 @@ export default function App() {
       <label for="my-modal-2" className="btn btn-primary modal-button">LEARN</label> 
       <input type="checkbox" id="my-modal-2" className="modal-toggle"> 
       <div className="modal">
-        <div className="modal-box">
-          <p>現在開発中。こうご期待!</p> 
-          <div className="modal-action">
-            <label for="my-modal-2" className="btn">Close</label>
-          </div>
-        </div>
+        a
       </div>
     </div>
   );
