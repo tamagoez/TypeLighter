8100df45e1f8cbf874fac5e8d4e26bbac400e881
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 21:06:24 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{jsx,tsx,js,ts}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ["Work Sans", ...fontFamily.sans]
-    },
     extend: {}
   },
   variants: {
