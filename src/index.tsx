b5905efe518725d4fdf771541188b729c01af7a7
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 18:46:03 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { render } from "react-dom";
 
 import App from "./App";
-import navbar from "./navbar":
 
 const rootElement = document.getElementById("root");
-render(<App />, <navbar />, rootElement);
+render(<App />, rootElement);
