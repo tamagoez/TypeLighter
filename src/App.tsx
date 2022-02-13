e4ccd1c2e330e82dbc9eb565dc1dcf7b4c1c33e2
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 21:14:12 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="modal-box">
-          <p>現在開発中。こうご期待!</p> 
-          <div className="modal-action">
-            <label for="my-modal-2" className="btn">Close</label>
-          </div>
-        </div>
+      <p className="font-sans">現在開発中。こうご期待!</p> 
     </div>
   );
 }
