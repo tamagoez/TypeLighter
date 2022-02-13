2edf5c75bb9101128370816056cba59850737e0f
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 21:04:51 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="flex w-auto space-x-4 flex-nowrap">
-        <div className="artboard phone-1 horizontal artboard-demo">
-          <h2>Start editing to see some magic happen!</h2>
-          <p className="font-bold">Hi</p>
-        </div>
-      </div>
       <label for="my-modal-2" className="btn btn-primary modal-button">LEARN</label> 
       <input type="checkbox" id="my-modal-2" className="modal-toggle"> 
       <div className="modal">
         <div className="modal-box">
-          <p>現在開発中のため、使用できません;; STARを付けて終了までお待ちください!</p> 
+          <p>現在開発中。こうご期待!</p> 
           <div className="modal-action">
             <label for="my-modal-2" className="btn">Close</label>
           </div>
