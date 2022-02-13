46b0cf700f2004a510467c61f265ca43d45b8b11
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 20:04:52 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p className="font-bold">Hi</p>
+      <div class="flex w-auto space-x-4 flex-nowrap">
+        <div class="artboard phone-1 horizontal artboard-demo">
+          <h2>Start editing to see some magic happen!</h2>
+          <p className="font-bold">Hi</p>
+        </div>
+      </div>
     </div>
   );
 }
