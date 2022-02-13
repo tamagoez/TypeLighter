8fd1231b310700d30df45bf259f9900da66125d6
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-13 20:09:28 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,17 @@ export default function App() {
           <p className="font-bold">Hi</p>
         </div>
       </div>
+      <label for="my-modal-2" className="btn btn-primary modal-button">open modal</label> 
+      <input type="checkbox" id="my-modal-2" className="modal-toggle"> 
+      <div className="modal">
+        <div className="modal-box">
+          <p>Enim dolorem dolorum omnis atque necessitatibus. Consequatur aut adipisci qui iusto illo eaque. Consequatur repudiandae et. Nulla ea quasi eligendi. Saepe velit autem minima.</p> 
+          <div className="modal-action">
+            <label for="my-modal-2" className="btn btn-primary">Accept</label> 
+            <label for="my-modal-2" className="btn">Close</label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
